7ea62a0118c587e1695a85f8022450d315562537
banuakman <banu@asterya.com>
2021-03-23 00:00:25 -0400

    connect to a MongoDB database using the mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -14,6 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 //Start Server
 app.listen(PORT, () => {
