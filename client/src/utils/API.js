12c33e151983ab9441d50b783e080d363ed04a06
banuakman <banu@asterya.com>
2021-03-30 17:59:10 -0400

    add axios api calls

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,6 +6,24 @@ const api = {
       `https://www.googleapis.com/books/v1/volumes?q=${query}`
     );
   },
+
+  // Saves a post to the database
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+
+  // Gets all posts
+  getBooks: function () {
+    return axios.get("/api/books");
+  },
+  // Gets the post with the given id
+  getBook: function (id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the post with the given id
+  deleteBook: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
 };
 
 export default api;
